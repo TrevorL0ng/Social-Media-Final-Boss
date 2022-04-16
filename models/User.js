43fbc59ac2b557c5c137afb55ee463613d3bf46b
Trevor L <trevorlong992@gmail.com>
2022-04-16 17:55:40 -0400

    Another big dumb, using wrong RegEx. Thanks Mongoose documentation. All working now. Just to build out the Insomnia routes and get the video up

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new Schema({
         type: String,
         unique: true,
         required:true,
-        match: [/.+@.!\..+/, 'Enter a valid email']
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
     },
     thoughts: [],
     friends: [this]
